9817dc83c623d2aa3fd14670910c027b58338e25
hondayuki0606 <hondayuki0606@yahoo.co.jp>
2025-01-12 21:51:16 +0900

    FSA-15, 初回起動用のPage作成

--- lib/stg_debug.dart
+++ lib/stg_debug.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:searchapplication/page/search_page.dart';
+import 'package:searchapplication/page/launch/launch_page.dart';
 import 'package:searchapplication/config.dart';
 
 void main() {
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const SearchPage(),
+      home: const LaunchPage(),
     );
   }
 }
